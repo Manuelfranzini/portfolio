4a3abf640d11aea6396d40e2b358246aa9fd47c8
Manuel Franzini <manuelfranzini@hotmail.ch>
2023-11-12 17:46:17 +0100

    12Nov23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './staticDB';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const Popup = ({ dataToShow, onClose }) => {
+    return (
+        <ul class="popup">
+            <div className="popup-content">
+                {Object.entries(dataToShow).map(([key, value]) => (
+                    <li className="list-point" key={key}>
+                        <div ><strong>{key} <br/> </strong> {Array.isArray(value) ? value.join(', ') : value}</div>
+                    </li>
+                ))}
+                <span class="close-popup" onClick={onClose}>
+                    &times;
+                </span>
+            </div>
+        </ul>
+    );
+};
+
+const App = () => {
+    const [selectedData, setSelectedData] = useState(null);
+    const [showButtons, setShowButtons] = useState(true);
+
+    const handleButtonClick = (index) => {
+        setSelectedData(data[index]);
+        setShowButtons(false);
+    };
+
+    const handleClosePopup = () => {
+        setSelectedData(null);
+        setShowButtons(true);
+    };
+
+    return (
+        <div>
+            {showButtons && (
+                <div>
+                    {data.map((item, index) => (
+                        <button className="button" key={index} onClick={() => handleButtonClick(index)}>
+                            {item.What}
+                        </button>
+                    ))}
+                </div>
+            )}
+            {!showButtons && (
+                <Popup dataToShow={selectedData} onClose={handleClosePopup} />
+            )}
+            <div class="background-text">Manuel Franzini</div>
+        </div>
+    );
+};
 
 export default App;
+
+/*
+        
+*/
---
+++ src/staticDB.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        What: 'About',
+        Origin: ['CH Switzerland', 'Bern'],
+        Education: ['Bürofachdiplom VSH', 'Handelsdiplom VSH', 'Certificate of higher education in Computing & IT and Business', 'BSc Computing & IT'],
+        Certificates: ['AZ-900', 'PL-900', 'PL-100', 'PL-200'],
+        Languages: ['Swiss-German native', 'German superior', 'English professional', 'French professional', 'Spanish limited'],
+    },
+    {
+        What: 'Project 1',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023-01-01',
+    },
+    {
+        What: 'Project 2',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        description: 'Consectetur adipiscing elit.',
+        date: '2023-02-01',
+    },
+];
+
+
+export default data;
